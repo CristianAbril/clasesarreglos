9ad90bb0cf2e1580f54cee058ec8764a714d5c74
CristianAbril <trishtan142@gmail.com>
2019-09-17 10:00:06 -0500

    1 file modified, 1 added

--- src/banda/banda.component.ts
+++ src/banda/banda.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import {Bandass}  from './bandass' ;
 
 @Component({
   selector: 'app-banda',
   templateUrl: './banda.component.html',
   styleUrls: ['./banda.component.css']
 })
-export class BandaComponent implements OnInit {
+export class BandaComponent {
+  banda1: Bandass;
+  banda2: Bandass;
+  banda3 :Bandass;
 
-  constructor() { }
+  constructor(){
+   this.banda1 =new Bandass("Harry","descpripcion", "xx");
+   this.banda2 =new Bandass("Harry", "descpriocin", "xx");
+   this.banda3 =new Bandass("Harry3","descripcion", "xx");
 
-  ngOnInit() {
   }
-
 }
---
+++ src/banda/bandass.ts
@@ -0,0 +1,11 @@
+export class Bandass {
+  nombre : string
+	descripcion : string
+	
+
+  constructor(nom : string, desc : string, url : string) {
+    this.nombre=nom
+		this.descripcion=desc
+	
+  }
+}
