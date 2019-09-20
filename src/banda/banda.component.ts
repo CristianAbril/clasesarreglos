e30f5aee9b6391307098f3e3d643a907f7d9f94b
CristianAbril <trishtan142@gmail.com>
2019-09-20 08:23:37 -0500

    3 files modified

--- src/banda/banda.component.ts
+++ src/banda/banda.component.ts
@@ -12,13 +12,7 @@ export class Artista1{
 
   constructor(){
   
-  this.artistas.push(new Artistaa("100","NoFeedBAck","10","1"));
-  this.artistas.push(new Artistaa("658","Shakira","8","1"));
-  this.artistas.push(new Artistaa("115","Cabas","6","2"));
-  this.artistas.push(new Artistaa("205","Beyonce","6","2"));
-  this.artistas.push(new Artistaa("326","ColdPlay","8","3"));
-  this.artistas.push(new Artistaa("107","DrKrapula","10","4"));
-  this.artistas.push(new  Artistaa("401","Juanes","2","4"));
+  
 
   }
   
