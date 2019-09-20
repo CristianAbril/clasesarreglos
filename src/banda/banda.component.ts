b7ca2087323c87c3e007c18918e4108a8f10be5a
CristianAbril <trishtan142@gmail.com>
2019-09-20 07:57:35 -0500

    2 files modified

--- src/banda/banda.component.ts
+++ src/banda/banda.component.ts
@@ -2,7 +2,7 @@ import { Component} from '@angular/core';
 import {Artistaa}  from './Artista';
 
 @Component({
-  selector: 'app-bandas',
+  selector: 'app-banda',
   templateUrl: './banda.component.html',
   styleUrls: ['./banda.component.css']
 })
@@ -11,14 +11,13 @@ export class Artista1{
   artistas:Artistaa[];
 
   constructor(){
-  this.artistas=new Artistaa[7]();
-  this.artistas[1]=new Artistaa("100","NoFeedBAck","10","1");
-  this.artistas[2]=new Artistaa("658","Shakira","8","1");
-  this.artistas[3]=new Artistaa("115","Cabas","6","2");
-  this.artistas[4]=new Artistaa("205","Beyonce","6","2");
-  this.artistas[5]=new Artistaa("326","ColdPlay","8","3");
-  this.artistas[6]=new Artistaa("107","DrKrapula","10","4");
-  this.artistas[7]=new  Artistaa("401","Juanes","2","4");
+  this.artistas.push(new Artistaa("100","NoFeedBAck","10","1"));
+  this.artistas.push(new Artistaa("658","Shakira","8","1"));
+  this.artistas.push(new Artistaa("115","Cabas","6","2"));
+  this.artistas.push(new Artistaa("205","Beyonce","6","2"));
+  this.artistas.push(new Artistaa("326","ColdPlay","8","3"));
+  this.artistas.push(new Artistaa("107","DrKrapula","10","4"));
+  this.artistas.push(new  Artistaa("401","Juanes","2","4"));
 
   }
   
