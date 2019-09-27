eca86f6b2f2629aff2f37f3f76178a6760587d87
CristianAbril <trishtan142@gmail.com>
2019-09-27 10:32:23 -0500

    2 files modified

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculadora',
   templateUrl: './calculadora.component.html',
-  styleUrls: ['./calculadora.component.css']
+  styleUrls: ['./calculadora.component.css'],
+  providers[CalculadoraService]
 })
 export class CalculadoraComponent implements OnInit {
 
-  constructor() { }
+  constructor(private calculadora: CalculadoraService) { }
 
   ngOnInit() {
   }
+  sumar():void{
+this.resultado= this.calculadora.sumar(this.a,this.b)
 
+  }
 }
