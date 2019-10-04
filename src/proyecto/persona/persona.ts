a7fd799335937fd231a7505d37d2005ed056a754
CristianAbril <trishtan142@gmail.com>
2019-10-04 13:38:17 -0500

    1 file modified

--- src/proyecto/persona/persona.ts
+++ src/proyecto/persona/persona.ts
@@ -6,11 +6,21 @@ estadocivil: string;
 boleano: boolean;
 idpublico: string;
 
+
+
+
 constructor(nombre: string, RH: string,sexo: string, estadocivil: string,boleano: boolean,idpublico: string){
 
-
+this.nombre=nombre;
+this.RH=RH;
+this.sexo=sexo;
+this.estadocivil=estadocivil;
+this.boleano=boleano;
+this.idpublico=idpublico;
 }
 
-
+ getidpublico():string{
+    return this.idpublico+" "+this.RH+" "+this.sexo+" "+this.estadocivil+" "+this.boleano+" ";
+  }
 
 }
