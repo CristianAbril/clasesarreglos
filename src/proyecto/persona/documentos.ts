64fb88453e0244fd863e145a287b4d385c152346
CristianAbril <trishtan142@gmail.com>
2019-10-04 13:52:29 -0500

    1 file modified, 1 added

---
+++ src/proyecto/persona/documentos.ts
@@ -0,0 +1,12 @@
+export class Documentos {
+
+iddocumento: string
+tipodocumento: string 
+ 
+ getiddocumento():string{
+    return this.iddocumento;
+  }
+   gettipodocumento():string{
+    return this.tipodocumento;
+  }
+}
--- src/proyecto/persona/persona.component.ts
+++ src/proyecto/persona/persona.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./persona.component.css']
 })
 export class PersonaComponent implements OnInit {
-
+  
+  
   constructor() { }
 
   ngOnInit() {
