e92a9a815dcd833c743c59478c65196bfb799cf8
CristianAbril <trishtan142@gmail.com>
2019-11-08 08:07:32 -0500

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,22 @@ import { DatabindingComponent } from './databinding/databinding.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
 import {VincularService }from '../proyecto/servicios/vincular.service';
 import{PersonaComponent} from '../proyecto/persona/persona.component';
+import {RouterModule, Routes} from '@angular/router';
+import { Componente1Component } from './componente1/componente1.component';
+import { Componente2Component } from './componente2/componente2.component' ;
 
 
+const appRoutes: Routes=[
+  {path: '', component:
+  Componente1Component},
+  {path:'Componente1', component:
+  Componente1Component},
+  {path:'Componente2/:id', component:
+  Componente2Component},];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, persona, pelicula, paciente1,Artista1,Empresa1, SaludoComponent, DatabindingComponent, CalculadoraComponent],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)
+ ],  declarations: [ AppComponent, HelloComponent, persona, pelicula, paciente1,Artista1,Empresa1, SaludoComponent, DatabindingComponent, CalculadoraComponent, Componente1Component, Componente2Component],
   bootstrap:    [ AppComponent ],
   providers: [ServicioService],
   
